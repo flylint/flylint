40723e2d8a55caa542e784f146aa60da6ad68466
USAMI Kenta <tadsan@zonu.me>
2019-09-28 17:46:38 +0900

    Init repository

---
+++ Cask
@@ -0,0 +1,2 @@
+(package "flylint" "0.0.1" "Asynchronous on-the-fly inspection framework")
+(source melpa)
---
+++ flylint.el
@@ -0,0 +1,38 @@
+;;; flylint.el --- Asynchronous on-the-fly inspection framework  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Flylint Community
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;;         USAMI Kenta <tadsan@zonu.me>
+;; Version: 0.0.1
+;; Keywords: tools
+;; Package-Requires: ((emacs "26"))
+;; License: GPL-3.0
+;; Homepage: https://github.com/flylint/flylint
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Flylint is alternative of Flymake and Flycheck.
+
+;;; Code:
+
+;;;###autoload
+(define-minor-mode flylint-mode
+  "Minor mode for asynchronous on-the-fly inspection."
+  )
+
+(provide 'flylint)
+;;; flylint.el ends here
