95f67fb114d2f66af2d2045a3a69554da3263fb3
conao3 <conao3@gmail.com>
2019-09-29 21:55:01 +0900

    fix comment for flylint-test.el

--- flylint-tests.el
+++ flylint-tests.el
@@ -1,29 +1,25 @@
-;;; flylint-tests.el ---                                -*- lexical-binding: t; -*-
+;;; flylint-tests.el --- Test definitions for flylint  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019  Naoya Yamashita
+;; Copyright (C) 2019  Flylint Community
 
-;; Author: Naoya Yamashita <conao3@gmail.com>
-;; Maintainer: Naoya Yamashita <conao3@gmail.com>
-;; URL: https://github.com/conao3/flylint.el
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the Affero GNU General Public License as
-;; published by the Free Software Foundation, either version 3 of the
-;; License, or (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Affero
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
-;; You should have received a copy of the Affero GNU General Public
-;; License along with this program.  If not, see
-;; <https://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;;
+;; Test definitions for `flylint'.
 
+
 ;;; Code:
 
 (load "cort-test")
