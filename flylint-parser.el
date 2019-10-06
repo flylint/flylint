f748e435550b41bde3b9193d084d8ed9688d7be0
conao3 <conao3@gmail.com>
2019-10-06 13:51:51 +0900

    add fllint-parser.el

---
+++ flylint-parser.el
@@ -0,0 +1,50 @@
+;;; flylint-parser.el --- Asynchronous on-the-fly inspection framework  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Flylint Community
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;;         USAMI Kenta <tadsan@zonu.me>
+;; License: GPL-3.0
+;; Homepage: https://github.com/flylint/flylint
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Flylint is alternative of Flymake and Flycheck.
+
+
+;;; Code:
+
+(defgroup flylint-parser nil
+  "Asynchronous on-the-fly inspection parser."
+  :prefix "flylint-parser"
+  :group 'tools)
+
+
+;;; Options
+
+
+;;; Functions
+
+
+;;; Main
+
+(provide 'flylint-parser)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; flylint-parser.el ends here
